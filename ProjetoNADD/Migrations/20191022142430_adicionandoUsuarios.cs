a284f4e1d7a2163a0bbc998d433ded542a174498
Hiago Vinicius <hiago149@gmail.com>
2019-10-22 12:15:02 -0300

    Criação Usuários

---
+++ ProjetoNADD/Migrations/20191022142430_adicionandoUsuarios.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoNADD.Migrations
+{
+    public partial class adicionandoUsuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id_Usuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Login_Usuario = table.Column<string>(nullable: false),
+                    Nome_Usuario = table.Column<string>(nullable: false),
+                    Senha_Usuario = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id_Usuario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ ProjetoNADD/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoNADD.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id_Usuario { get; set; }
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        public string Login_Usuario { get; set; }
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        public string Nome_Usuario { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Senha_Usuario { get; set; }
+    }
+}
