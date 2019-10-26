f21c20a628ace193201f2bfb06c74864cae8e96a
Hiago Vinicius <hiago149@gmail.com>
2019-10-26 12:44:04 -0300

    Campos Adicionais Avaliações

---
+++ ProjetoNADD/Migrations/20191026144325_CampoClarezaAvaliacao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoNADD.Migrations
+{
+    public partial class CampoClarezaAvaliacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Clareza_Avaliacao",
+                table: "Avaliacao",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Clareza_Avaliacao",
+                table: "Avaliacao");
+        }
+    }
+}
---
+++ ProjetoNADD/Migrations/20191026144741_CampoComplexidadeAvaliacao.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoNADD.Migrations
+{
+    public partial class CampoComplexidadeAvaliacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Complexidade_Avaliacao",
+                table: "Avaliacao",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Complexidade_Avaliacao",
+                table: "Avaliacao");
+        }
+    }
+}
