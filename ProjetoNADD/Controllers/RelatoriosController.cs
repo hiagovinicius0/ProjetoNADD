ff15b1c552d6b5b43f18a760a17e86b6ff0415f3
Hiago Vinicius <hiago149@gmail.com>
2019-10-26 12:44:19 -0300

    Relatório de Avaliações

---
+++ ProjetoNADD/Controllers/RelatoriosController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using ProjetoNADD.Data;
+using System.Linq;
+
+namespace ProjetoNADD.Controllers
+{
+    [Authorize]
+    public class RelatoriosController : Controller
+    {
+
+        private readonly ProjetoNADDContext _context;
+
+        public RelatoriosController(ProjetoNADDContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Avaliacoes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public object GetCursos()
+        {
+            var query = _context.Curso.Select(st => new {
+                Id = st.Id_Curso,
+                Nome = st.Nome_Curso
+            }).ToList();
+            return query;
+        }
+        [HttpPost]
+        public object BuscaDisciplinas(int Id_Curso)
+        {
+            var query = _context.Disciplina.Where(d => d.CursoId == Id_Curso).Select(d => new
+            {
+                Id = d.Id_Disciplina,
+                Nome = d.Nome_Disciplina
+            }).ToList();
+            return query;
+        }
+        [HttpPost]
+        public object BuscaRelatorio(int Id_Curso, int Id_Disciplina)
+        {
+            var query = from av in _context.Avaliacao
+                        join d in _context.Disciplina on av.DisciplinaId equals d.Id_Disciplina
+                        join c in _context.Curso on d.CursoId equals c.Id_Curso
+                        join ar in _context.Area on c.AreaId equals ar.Id_Area
+                        where c.Id_Curso == Id_Curso
+                        where d.Id_Disciplina == Id_Disciplina
+                        select new
+                        {
+                            Nome = av.Nome_Avaliacao,
+                            Ano = d.Ano_Disciplina,
+                            Curso = c.Nome_Curso,
+                            Periodo = d.Periodo_Disciplina,
+                            Area = ar.Nome_Area,
+                            Contextualidade = av.Contextualidade_Avaliacao,
+                            Complexidade = av.Complexidade_Avaliacao,
+                            Clareza = av.Clareza_Avaliacao
+                        };
+            return query;                
+        }
+    }
+}
---
+++ ProjetoNADD/wwwroot/js/Relatorios/Avaliacoes.js
@@ -0,0 +1,109 @@
+﻿function GetCursos() {
+    $.ajax({
+        type: "POST",
+        url: "../Relatorios/GetCursos",
+        success: function (dados) {
+            $('#Id_Curso').html('');
+            $('#Id_Curso').append('<option value =""></option>');
+            if (dados.length > 0) {
+                var limite = dados.length;
+                for (var i = 0; i < limite; i++) {
+                    $('#Id_Curso').append('<option value ="'+dados[i].id+'">'+dados[i].nome+'</option>');
+                }
+            }
+            $("#Id_Curso").select2({
+                placeholder: "Selecione o Curso"
+            });
+        }
+    });
+}
+function BuscaDisciplinas() {
+    var curso = $('#Id_Curso').val();
+    $.ajax({
+        type: "POST",
+        url: "../Relatorios/BuscaDisciplinas",
+        data: {
+            Id_Curso: curso
+        },
+        success: function (dados) {
+            $('#Id_Disciplina').html('');
+            $('#Id_Disciplina').append('<option value =""></option>');
+            if (dados.length > 0) {
+                var limite = dados.length;
+                for (var i = 0; i < limite; i++) {
+                    $('#Id_Disciplina').append('<option value ="' + dados[i].id + '">' + dados[i].nome + '</option>');
+                }
+            }
+            $("#Id_Disciplina").select2({
+                placeholder: "Selecione a Disciplina"
+            });
+            $('#divDIsciplinas').css('display', 'block')
+        }
+    });
+}
+function BuscaRelatorio() {
+    var disciplina = $('#Id_Disciplina').val();
+    var curso = $('#Id_Curso').val();
+    if (disciplina === "") {
+        $('#conteudo_relatorio').css('display', 'none')
+        return;
+    }
+    if (curso === "") {
+        $('#conteudo_relatorio').css('display', 'none')
+        return;
+    }
+    if ($.fn.DataTable.isDataTable('#tabelaRelatorio')) {
+        $('#tabelaRelatorio').DataTable().destroy();
+    }
+    $.ajax({
+        type: "POST",
+        url: "../Relatorios/BuscaRelatorio",
+        data: {
+            Id_Curso: curso,
+            Id_Disciplina: disciplina
+        },
+        success: function (dados) {
+            $('#corpoTabelaRelatorio').html('');
+            console.log(dados)
+            if (dados.length > 0) {
+                var limite = dados.length
+                for (var i = 0; i < limite; i++) {
+                    var contextualizacao = dados[i].contextualidade !== true ? "Sim" : "Não"
+                    var clareza = dados[i].clareza === true ? "Sim" : "Não"
+                    var complexidade = dados[i].complexidade !== null ? dados[i].complexidade : ""
+                    $('#corpoTabelaRelatorio').append('<tr><td>' + dados[i].nome + '</td><td>' + dados[i].ano + '</td><td>' + dados[i].curso + '</td><td>' + dados[i].periodo + '</td><td>' + dados[i].area + '</td><td>' + contextualizacao + '</td><td>' + clareza + '</td><td>' + complexidade +'</td></tr>');
+                }
+            }
+            $("#tabelaRelatorio").DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf'
+                ],
+                "language": {
+                    "sEmptyTable": "Nenhum registro encontrado",
+                    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                    "sInfoPostFix": "",
+                    "sInfoThousands": ".",
+                    "sLengthMenu": "_MENU_ resultados por página",
+                    "sLoadingRecords": "Carregando...",
+                    "sProcessing": "Processando...",
+                    "sZeroRecords": "Nenhum registro encontrado",
+                    "sSearch": "Pesquisar",
+                    "oPaginate": {
+                        "sNext": "Próximo",
+                        "sPrevious": "Anterior",
+                        "sFirst": "Primeiro",
+                        "sLast": "Último"
+                    },
+                    "oAria": {
+                        "sSortAscending": ": Ordenar colunas de forma ascendente",
+                        "sSortDescending": ": Ordenar colunas de forma descendente"
+                    }
+                }
+            });
+            $('#conteudo_relatorio').css('display', 'block')
+        }
+    });
+}
