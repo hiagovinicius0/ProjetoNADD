093c07dea7338edad332f4d5064a1d45c866e80b
Hiago Vinicius <hiago149@gmail.com>
2019-10-30 12:53:41 -0300

    Tipo Questão multiplo

---
+++ ProjetoNADD/Migrations/20191030154129_TipoQuestao.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoNADD.Migrations
+{
+    public partial class TipoQuestao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TipoID",
+                table: "Questao",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoQuestaoId_TipoQuestao",
+                table: "Questao",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestoesMEeD_Avaliacao",
+                table: "Avaliacao",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.CreateTable(
+                name: "TipoQuestao",
+                columns: table => new
+                {
+                    Id_TipoQuestao = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome_TipoQuestao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoQuestao", x => x.Id_TipoQuestao);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questao_TipoQuestaoId_TipoQuestao",
+                table: "Questao",
+                column: "TipoQuestaoId_TipoQuestao");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questao_TipoQuestao_TipoQuestaoId_TipoQuestao",
+                table: "Questao",
+                column: "TipoQuestaoId_TipoQuestao",
+                principalTable: "TipoQuestao",
+                principalColumn: "Id_TipoQuestao",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questao_TipoQuestao_TipoQuestaoId_TipoQuestao",
+                table: "Questao");
+
+            migrationBuilder.DropTable(
+                name: "TipoQuestao");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questao_TipoQuestaoId_TipoQuestao",
+                table: "Questao");
+
+            migrationBuilder.DropColumn(
+                name: "TipoID",
+                table: "Questao");
+
+            migrationBuilder.DropColumn(
+                name: "TipoQuestaoId_TipoQuestao",
+                table: "Questao");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "QuestoesMEeD_Avaliacao",
+                table: "Avaliacao",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ ProjetoNADD/Models/TipoQuestao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoNADD.Models
+{
+    public class TipoQuestao
+    {
+        [Key]
+        public int Id_TipoQuestao { get; set; }
+        public string Nome_TipoQuestao { get; set; }
+        public ICollection<Questao> Questao { get; set; }
+    }
+}
